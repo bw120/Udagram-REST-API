550bdb1207d22d1333ca18f7bc65a1154cfcbd54
Benjamin Wright <benjamin.wright@endurance.com>
2021-02-27 14:48:51 -0500

    implemented the generate and compare password functions using bcrypt

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -11,13 +11,15 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return "NotYetImplemented"
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
+    return bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
@@ -31,13 +33,13 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // if (!req.headers || !req.headers.authorization){
     //     return res.status(401).send({ message: 'No authorization headers.' });
     // }
-    
+
 
     // const token_bearer = req.headers.authorization.split(' ');
     // if(token_bearer.length != 2){
     //     return res.status(401).send({ message: 'Malformed token.' });
     // }
-    
+
     // const token = token_bearer[1];
 
     // return jwt.verify(token, "hello", (err, decoded) => {
@@ -48,8 +50,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // });
 }
 
-router.get('/verification', 
-    requireAuth, 
+router.get('/verification',
+    requireAuth,
     async (req: Request, res: Response) => {
         return res.status(200).send({ auth: true, message: 'Authenticated.' });
 });
